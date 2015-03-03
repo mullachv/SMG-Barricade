f515a059672138b46886f041426f007b181d0b58
Wuping Lei <wuping.lei@nyu.edu>
2015-03-03 16:23:29 -0500

    add some tests

--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function(config){
       'http://ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular-mocks.js',
       'gameLogic.js',
       'gameLogic_test.js',
+      'gameLogictest_ByRay.js',
       //'http://yoav-zibin.github.io/emulator/alphaBetaService.js',
       //'aiService.js',
       //'aiService_test.js'
