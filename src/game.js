a9b3ef536bcebbd0cb9182bb4ef7f1434beaef29
hui-yang <hy821@nyu.edu>
2015-03-24 14:08:00 -0400

    fix dice

--- src/game.js
+++ src/game.js
@@ -57,7 +57,7 @@ angular.module('myApp')
                 };
                 $scope.getImageSrc = function (row, col) {
                     var cell = $scope.board[row][col];
-                    var dice = $scope.stateAfterMove.dice;
+                    var dice = $scope.dice;
                     if (row === 0 && col === 8 && cell === "W") {
                         return "imgs/WinningSpot.png";
                     }
