0d9e5662df196329aa17d5d78d7525dd73ab2c63
hui-yang <hy821@nyu.edu>
2015-03-25 12:14:41 -0400

    fix controller

--- src/game.js
+++ src/game.js
@@ -8,12 +8,24 @@ angular.module('myApp')
                 resizeGameAreaService.setWidthToHeight(1.0625);
 
                 function sendComputerMove() {
-                    gameService.makeMove(gameLogic.createRandomPossibleMove($scope.board, $scope.turnIndex,
-                            // at most 1 second for the AI to choose a move (but might be much quicker)
-                            {millisecondsLimit: 1000}));
+                    var turnIndex = $scope.turnIndex;
+                    sendDiceMove();
+
+                    gameService.makeMove(gameLogic.createRandomPossibleMove($scope.board,
+                        "normal", $scope.dice, $scope.turnIndex));
+
+                    if ($scope.turnIndex === turnIndex) {
+                        gameService.makeMove(gameLogic.createRandomPossibleMove($scope.board,
+                            "barricade", -1, $scope.turnIndex));
+                    }
+                }
+
+                function sendDiceMove() {
+                  gameService.makeMove(gameLogic.createDiceMove($scope.dice, $scope.turnIndex));
                 }
 
                 function updateUI(params) {
+                    var lastType = params.stateAfterMove.type;
                     $scope.board = params.stateAfterMove.board;
                     $scope.delta = params.stateAfterMove.delta;
                     if ($scope.board === undefined) {
@@ -22,6 +34,16 @@ angular.module('myApp')
                     $scope.isYourTurn = params.turnIndexAfterMove >= 0 && // game is ongoing
                         params.yourPlayerIndex === params.turnIndexAfterMove; // it's my turn
                     $scope.turnIndex = params.turnIndexAfterMove;
+                    if (params.turnIndexBeforeMove !== params.turnIndexAfterMove) {
+                      $scope.dice = null;
+                      $scope.typeExpected = "dice";
+                    } else {
+                        if (lastType === "normal") {
+                            $scope.typeExpected = "barricade";
+                        } else {
+                            $scope.typeExpected = "normal";
+                        }
+                    }
 
                     // Is it the computer's turn?
                     if ($scope.isYourTurn &&
@@ -30,10 +52,16 @@ angular.module('myApp')
                         // Waiting 0.5 seconds to let the move animation finish; if we call aiService
                         // then the animation is paused until the javascript finishes.
                         $timeout(sendComputerMove, 500);
+                    } else {
+                        if ($scope.dice = null) {
+                            $timeout(sendDiceMove, 500);
+                        }
                     }
                 }
                 //window.e2e_test_stateService = stateService; // to allow us to load any state in our e2e tests.
 
+                var prev_row = null;
+                var prev_col = null;
                 $scope.cellClicked = function (row, col) {
                     $log.info(["Clicked on cell:", row, col]);
                     if (window.location.search === '?throwException') { // to test encoding a stack trace with sourcemap
@@ -42,13 +70,50 @@ angular.module('myApp')
                     if (!$scope.isYourTurn) {
                         return;
                     }
-                    try {
-                        var move = gameLogic.createMove($scope.board, row, col, $scope.turnIndex);
-                        $scope.isYourTurn = false; // to prevent making another move
-                        gameService.makeMove(move);
-                    } catch (e) {
-                        $log.info(["Cell is already full in position:", row, col]);
+                    if (!$scope.dice) { // wait until dice rolls
                         return;
+                    }
+
+                    // normal move, get a pawn and place it.
+                    if ($scope.typeExpected === "normal") {
+                        // choose the start point
+                        if (prev_row === null || prev_col === null) {
+                            if ($scope.shouldShowImage(row, col) &&
+                                $scope.board[row][col] !== 'W' &&
+                                $scope.board[row][col] !== '0') {
+                                prev_row = row;
+                                prev_col = col;
+                            }
+                            return;
+                        } else {
+                            try { // choose the end point
+                              // to prevent making another move
+                                $scope.isYourTurn = false;
+                                gameService.makeMove(gameLogic.createMove(
+                                    $scope.board, "normal", $scope.dice, row, col,
+                                        prev_row, prev_col, $scope.turnIndex));
+                                prev_row = null;
+                                prev_col = null;
+                            } catch (e) {
+                                $log.info(["Illegal move from ", row, col, " to ", prev_row, prev_col]);
+                                return;
+                            }
+                        }
+                    } else if ($scope.typeExpected === "barricade"){
+                        if (prev_row === null || prev_col === null) {
+                            $log.info(["Place a barricade at:", row, col]);
+                            prev_row = null;
+                            prev_col = null;
+                        }
+                        try {
+                            $scope.isYourTurn = false;
+                            gameService.makeMove(gameLogic.createMove(
+                                $scope.board, "barricade", $scope.dice, row, col,
+                                    -1, -1, $scope.turnIndex));
+                        } catch (e) {
+                            $log.info(["Illegal to place a barricade at:", row, col]);
+                            return;
+                        }
                     }
                 };
                 $scope.shouldShowImage = function (row, col) {
@@ -66,23 +131,23 @@ angular.module('myApp')
                     }
 
                     if (row === 15 && col === 8) {
-                      switch(dice) {
-                        case 1:
-                          return 'imgs/1.png';
-                        case 2:
-                          return 'imgs/2.png';
-                        case 3:
-                          return 'imgs/3.png';
-                        case 4:
-                          return 'imgs/4.png';
-                        case 5:
-                          return 'imgs/5.png';
-                        case 6:
-                          return 'imgs/6.png';
-                        default:
-                          //console.log('Error: dice out of range ' + $scope.dice);
-                          return 'imgs/6.png';
-                      }
+                        switch(dice) {
+                            case 1:
+                                return 'imgs/1.png';
+                            case 2:
+                                return 'imgs/2.png';
+                            case 3:
+                                return 'imgs/3.png';
+                            case 4:
+                                return 'imgs/4.png';
+                            case 5:
+                                return 'imgs/5.png';
+                            case 6:
+                                return 'imgs/6.png';
+                            default:
+                                //console.log('Error: dice out of range ' + $scope.dice);
+                                return 'imgs/6.png';
+                        }
                     }
                     return cell === "0" ? "imgs/EmptySpot.png"
                             : cell === "1" ? "imgs/Barricade.png"
@@ -99,7 +164,7 @@ angular.module('myApp')
                 gameService.setGame({
                     gameDeveloperEmail: "hy821@nyu.edu",
                     minNumberOfPlayers: 2,
-                    maxNumberOfPlayers: 4,
+                    maxNumberOfPlayers: 2,
                     isMoveOk: gameLogic.isMoveOk,
                     updateUI: updateUI
                 });
